09f49b0261ce2e5cda14397d9742ad9bbb0c041c
Muawiya-Ismael <116707240+Muawiya-Ismael@users.noreply.github.com>
2023-12-13 20:58:45 +0200

    Update App.js
    I forget to delete somting from App.js file so I edit it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
-import { useEffect, useState } from "react";
-import { collection, getDocs } from "@firebase/firestore";
-import { db , storage } from "./firebase";
 
-import {ref,uploadBytes,getDownloadURL,listAll,list, } from "firebase/storage";
-import { initializeApp } from "firebase/app";
-import GroupCard from "./components/Group Card/GroupCard";
 
 
 function App() {
@@ -12,7 +6,7 @@ function App() {
 
     return (
         <div className="App">
-            <div><GroupCard/></div>
+            <div>Hi</div>
         </div>
     );
 }
