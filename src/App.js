09625110c149c26b0c7b5bc35851e39cd3e2d8c3
Bushra <boshra.o.hourani@gmail.com>
2023-12-22 03:10:51 +0200

    I add the subNav task

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
+import React, { useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
+import Pages from "./components/pages.js";
+import "./components/style.css";
 
+const App = () => {
+  const [activePage, setActivePage] = useState("home");
 
+  const handleNavClick = (page) => {
+    console.log(`Navigating to ${page}`);
+    setActivePage(page);
+  };
 
-function App() {
+  useEffect(() => {
+    const pages = document.querySelectorAll(".page");
+    pages.forEach((page) => {
+      page.style.display = "none";
+    });
+    document.getElementById(activePage).style.display = "block";
+  }, [activePage]);
 
-
-    return (
-        <div className="App">
-            <div>Hi</div>
-        </div>
-    );
-}
+  return (
+    <div>
+      <Navbar handleNavClick={handleNavClick} activePage={activePage} />
+      <Pages activePage={activePage} />
+    </div>
+  );
+};
 
 export default App;
